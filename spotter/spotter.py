458cd3ef4772ac206b4daa28574a95685a8e3985
CKolean <datahaavi@outlook.com>
2022-01-17 01:17:01 +0200

    Testing label editing

--- spotter/spotter.py
+++ spotter/spotter.py
@@ -1,12 +1,33 @@
 from PySide2 import QtCore
 from PySide2.QtWidgets import *
 from PySide2 import QtWidgets
+import os
+
 # it's good practice to have ui as it's own module
 import spotter_ui as sui
 # needed for windows
+class app(sui.SpotterDialog):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.name = "none"
+        self.create_connections()
+
+    def show_text(self,text):
+        self.output.setText(text)
+    def do_search(self):
+        results = []
+        list = os.scandir("C:/Users/Thuule//Documents/maya/scripts")
+        for entry in list:
+            if entry.is_dir() or entry.is_file():
+                results.append(entry)
+        list.close()
+        self.show_text(str(results))
+
+    def create_connections(self):
+        self.searchBtn.clicked.connect(self.do_search)
 spotter = QApplication([])
 spotter.setStyle('Fusion')
 
-window = sui.SpotterDialog()
+window = app()
 window.show()
 spotter.exec_()
---
+++ spotter/spotter_ui.py
@@ -0,0 +1,57 @@
+from PySide2 import QtCore
+from PySide2.QtGui import QPalette, QColor
+from PySide2 import QtWidgets
+
+
+class SpotterDialog(QtWidgets.QDialog):
+    # using super to combine my init with parent class init
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setWindowTitle("Spotter")
+        self.setMinimumWidth(500)
+        self.setWindowFlags(self.windowFlags() ^ QtCore.Qt.WindowContextHelpButtonHint)
+        self.set_colors()
+        self.create_widgets()
+        self.create_layouts()
+    def set_colors(self):
+        self.setStyleSheet("color:rgb(219, 212, 204);"
+                           "background-color: rgb(36, 36, 36);"
+                           "selection-color: red;"
+                           "label-color: orange;")
+        #self.setStyleSheet("Window,QLineEdit,QPushButton { background-color: rgb(36, 36, 36) }")
+        self.setAutoFillBackground(True)
+
+    def create_widgets(self):
+        self.searchBtn = QtWidgets.QPushButton("Search!")
+        self.searchBtn.setStyleSheet("font: bold 14px")
+        self.input = QtWidgets.QLineEdit()
+        self.output = QtWidgets.QLabel("thing")
+
+        self.resultTable = QtWidgets.QTableWidget()
+        self.resultTable.setColumnCount(3)
+        self.resultTable.setColumnWidth(0, 300)
+        self.resultTable.setStyleSheet("color: white;"
+                        "background-color: rgb(36, 36, 36)"
+                        "selection-color: rgb(36, 36, 36)"
+                        "selection-background-color: black;")
+        self.resultTableHeader = self.resultTable.horizontalHeader()
+        self.resultTableHeader.setSectionResizeMode(1, QtWidgets.QHeaderView.Stretch)
+        self.resultTable.setHorizontalHeaderLabels(["Name", "Hits", "Time"])
+
+    def create_layouts(self):
+        main_layout = QtWidgets.QVBoxLayout(self)
+        button_layout = QtWidgets.QVBoxLayout(self)
+        button_layout.addStretch()
+
+        button_layout.addWidget(self.searchBtn)
+        button_layout.addWidget(self.input)
+
+        table_layout = QtWidgets.QVBoxLayout(self)
+        button_layout.addWidget(self.resultTable)
+
+        main_layout.addLayout(button_layout)
+        main_layout.addLayout(table_layout)
+        main_layout.addWidget(self.output)
+
+
+
